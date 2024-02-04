a170b855c1f698b8adac2e8f0ed760b6c2fdcccb
Cleiber Gustavo Soares Rodrigues Junior <42981722+cgsrjunior@users.noreply.github.com>
2024-02-04 20:14:42 -0300

    Update rita-preprocessing.r
    Put the last changes made in my working PC on Github
    
    Update preprocessing to avoid excluding Submission.ID

--- scripts/rita-preprocessing.r
+++ scripts/rita-preprocessing.r
@@ -1,7 +1,21 @@
+#*******************************************************************************
+#* Disclaimer: this code is an modified version of the following code cited below
+#* 
+#*    Title: Bibliometric analysis of published research in Revista
+#*           de Informática Teórica e Aplicada (RITA) from 2008 to 2021
+#*    Author: M Villalobos-Cid
+#*    Date: 2023
+#*    Code version: 1.0
+#*    Availability: https://rpubs.com/mvillalobos/Rita
+#*
+#*******************************************************************************
+
 library("stringr")    #Contiene las funciones para manejo de cadenas de texto
 
 # Lectura de datos originales
-data=read.csv("D:/metadata-article-ml/dataset/BD_17_11_2023.csv",sep=",",header=T)
+data=read.csv("C://Users/ntu_c/metadata-article-ml/dataset/BD_17_11_2023.csv",sep=",",header=T, row.names = NULL)
+
+#Fix header names error
 
 #Let's using a copy for comparison purposes
 data_filtered = data
@@ -9,7 +23,7 @@ data_filtered = data
 # Remover campos vacíos o incompletos\
 # Get column names that contain the substring desired to remove
 # Identify columns with all missing values
-id_columns <- grep("ID", names(data_filtered), value = TRUE)
+id_columns <- grep("ORCID", names(data_filtered), value = TRUE)
 email_columns <- grep("Email", names(data_filtered), value = TRUE)
 homepage_columns <- grep("Homepage", names(data_filtered), value = TRUE)
 biostatement_columns <- grep("Bio.Statement..e.g", names(data_filtered), value = TRUE)
@@ -53,4 +67,4 @@ for (col in affiliation_columns) {
 
 
 #Saving Files
-write.table(data_filtered,file="D:/metadata-article-ml/dataset/BD_RITA01.csv",quote = F,row.names = F,sep=";")
+write.table(data_filtered,file="C://Users/ntu_c/metadata-article-ml/dataset/BD_RITA01_new.csv",quote = F,row.names = F,sep=";")
